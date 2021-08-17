c135d1b1b10f19002ad069c47192c775d7f19621
Ona <onushkis@gmail.com>
2021-08-17 14:33:29 +0200

    add an assigment 1b

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -1,6 +1,51 @@
+
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
+
+	// 
+
+	let numberMax = 10;
+	let numbMin = 2;
+	let number = 5;
+	let textCountElement = document.getElementById("text-count");
+	let plusBtn = document.getElementById("btn-count-increase");
+	let minusBtn = document.getElementById("btn-count-decrease");
+
+
+function updateNumber(){
+
+textCountElement.textContent = number;
+	}
+	updateNumber();
+ plusBtn.addEventListener('click', () => {
+if(number < numberMax ) { 
+	number++;
+	updateNumber();
+}
+});
+
+minusBtn.addEventListener('click', () => {
+if (number > numbMin){ 
+	number--;
+	updateNumber();
+}
+});
+
+	
+
+	
+ 
+
+
+
+
+	
+	
+
+	
+	
+
 
 }); // Afslutter: DOMContentLoaded
