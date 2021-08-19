cbd5d322625a5bf86de95adac9edd1d13f483af8
Ona <onushkis@gmail.com>
2021-08-19 10:08:36 +0200

    add assigment 01a

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -1,51 +1,29 @@
-
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
+  // skriv din kode her...
 
-	// skriv din kode her...
+  //
 
-	// 
+  let number = 5;
+  let textCountElement = document.getElementById("text-count");
+  let plusBtn = document.getElementById("btn-count-increase");
+  let minusBtn = document.getElementById("btn-count-decrease");
 
-	
-	let number = 5;
-	
-	let textCountElement = document.getElementById("text-count");
-	let plusBtn = document.getElementById("btn-count-increase");
-	let minusBtn = document.getElementById("btn-count-decrease");
+  function updateNumber() {
+    textCountElement.textContent = number;
+  }
+  updateNumber();
+  plusBtn.addEventListener("click", () => {
+    if (number < 10) {
+      number++;
+      updateNumber();
+    }
+  });
 
-
-function updateNumber(){
-
-textCountElement.textContent = number;
-	}
-	updateNumber();
- plusBtn.addEventListener('click', () => {
-if(number < 10 ) { 
-	number++;
-	updateNumber();
-}
-});
-
-minusBtn.addEventListener('click', () => {
-if (number > 0){ 
-	number--;
-	updateNumber();
-}
-});
-
-	
-
-	
- 
-
-
-
-
-	
-	
-
-	
-	
-
-
+  minusBtn.addEventListener("click", () => {
+    if (number > 0) {
+      number--;
+      updateNumber();
+    }
+  });
 }); // Afslutter: DOMContentLoaded
